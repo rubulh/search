d008b89f80064cec235f100dc2d0975e501e5718
rubul haloi <rubulhaloi@yahoo.co.in>
2012-06-03 05:05:12 -0700

    the iteration support added for the program so now for a very small input the program ll give zero errors and i have set the number of correction iteraions as 50 maybe i ll need to decrease that on the instruction  of the mentor ;so over all the program is working fine and all the supports are included
    so in order to change anything and make it up for any input one needs to
    >change the parity check matrix
    >change the codeword and takecare that the codeword actually matches with
    >the dimensions of the the parity check matrix
    > and if required change the number of iterations

--- thesecondpart.m
+++ thesecondpart.m
@@ -1,7 +1,13 @@
 H=[ 1 1 0 1 0 0; 1 1 1 0 0 1 ; 0 1 1 0 1 0 ]
 h_size=size(H);
-the_codeword=[0 1 1 1 0 1]
-the_cross_over_probability=[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9];
+the_codeword=[0 0 0 0 0 0]
+
+%form the vector for cross over probablilty
+the_cross_over_probability=zeros(0,99);
+for k_k=1:99
+    the_cross_over_probability(k)=0.01*k;
+end
+%the cross over probability formed
 errors=zeros(numel(the_cross_over_probability));
 'next is tcop looping'
 for tcop=1:numel(the_cross_over_probability)
@@ -10,46 +16,64 @@ for tcop=1:numel(the_cross_over_probability)
     the_bsc_codeword=bsc(the_codeword,the_cross_over_probability(tcop));
     the_bsc_codeword
     the_final_codeword=the_bsc_codeword;
-
-    the_indicator_matrix=zeros(h_size(1),h_size(2));
-
-    for i=1:h_size(1)
-        i_row_ones= find(H(i,:));
-        %find all the connections in to the checknode i
-        the_i_row_xor=0;
-        for k=1:numel(i_row_ones)
-            the_i_row_xor=xor(the_i_row_xor,the_bsc_codeword(i_row_ones(k)));
-        end
-        for j=1:numel(i_row_ones)
-            the_indicator_matrix(i,i_row_ones(j))=xor(the_i_row_xor,the_bsc_codeword(i_row_ones(j)));
-        end
-    end
+    the_last_final_keyword=the_bsc_codeword;
     
-    for i=1:h_size(2)
-        i_i_row_ones=find(H(:,i));
-        the_sum_of_the_ones=0;
-        for j=1:numel(i_i_row_ones)
-            the_sum_of_the_ones=the_sum_of_the_ones+the_indicator_matrix(i_i_row_ones(j),i);
-        end
-        if(rem(numel(i_i_row_ones),2)==0)
-             if((int8(numel(i_i_row_ones)/2))<the_sum_of_the_ones)
-                 the_final_codeword(i)=1;
-             end
-            if((int8(numel(i_i_row_ones)/2))>the_sum_of_the_ones)
-                 the_final_codeword(i)=0;
+    
+    
+%see this line
+    for x=1:50
+        '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$-------iterations-------$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+        x
+        the_indicator_matrix=zeros(h_size(1),h_size(2));
+        the_last_final_keyword=the_final_codeword;
+        for i=1:h_size(1)
+            i_row_ones= find(H(i,:));
+            %find all the connections in to the checknode i
+            the_i_row_xor=0;
+            for k=1:numel(i_row_ones)
+                the_i_row_xor=xor(the_i_row_xor,the_final_codeword(i_row_ones(k)));
+            end
+            for j=1:numel(i_row_ones)
+                the_indicator_matrix(i,i_row_ones(j))=xor(the_i_row_xor,the_final_codeword(i_row_ones(j)));
             end
         end
+    
+        for i=1:h_size(2)
+            i_i_row_ones=find(H(:,i));
+            the_sum_of_the_ones=0;
+            for j=1:numel(i_i_row_ones)
+                the_sum_of_the_ones=the_sum_of_the_ones+the_indicator_matrix(i_i_row_ones(j),i);
+            end
+            if(rem(numel(i_i_row_ones),2)==0)
+                 if((int8(numel(i_i_row_ones)/2))<the_sum_of_the_ones)
+                     the_final_codeword(i)=1;
+                 end
+                if((int8(numel(i_i_row_ones)/2))>the_sum_of_the_ones)
+                     the_final_codeword(i)=0;
+                end
+            end
     
    
-        if(rem(numel(i_i_row_ones),2)==1)
-             if((int8(numel(i_i_row_ones)/2)-1)<the_sum_of_the_ones)
-                 the_final_codeword(i)=1;
-             end
-            if((int8(numel(i_i_row_ones)/2)-1)>=the_sum_of_the_ones)
-                 the_final_codeword(i)=0;
+            if(rem(numel(i_i_row_ones),2)==1)
+                 if((int8(numel(i_i_row_ones)/2)-1)<the_sum_of_the_ones)
+                     the_final_codeword(i)=1;
+                 end
+                if((int8(numel(i_i_row_ones)/2)-1)>=the_sum_of_the_ones)
+                     the_final_codeword(i)=0;
+                end
             end
         end
+        the_final_codeword
+        if(the_final_codeword==the_last_final_keyword)
+            'there it matches>>>>>>>>>>>>>>>>>>>'
+            the_final_codeword
+            the_last_final_keyword
+            break;
+        end
+    %upto here the final codeword formation is finished
+        '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$-------iterations-over------$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     end
+    
     
     %calculate the number of errors
     for o=1:h_size(2)
@@ -57,8 +81,6 @@ for tcop=1:numel(the_cross_over_probability)
             errors(tcop)=errors(tcop)+1;
         end
     end
-    the_final_codeword
-    errors(tcop)
     '************************END*******************************'
 end
 plot(the_cross_over_probability,errors)
