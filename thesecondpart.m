4422c5cf7e1a3010ed71e9f895025ba941d8c88f
rubul haloi <rubulhaloi@yahoo.co.in>
2012-06-03 03:24:20 -0700

    support for graphing against the crossover probaility versus errors enabled ;for now i am using an array for cross overprobability by hardcoding itt;later i shall make the array usning a for loop or something
    one major change compared to the last version is that i am calculating
    errors and am adding them to the errors (indexed by the tcop)  with each
    error found indexed by the codeword to o
    
    but notable that the algorith should be iterative and is not at this
    time;;now all i am doing is that correcting the bsced codeword for an
    instance and not iterating over it; this need to be changed to get better
    correction

--- thesecondpart.m
+++ thesecondpart.m
@@ -1,44 +1,64 @@
 H=[ 1 1 0 1 0 0; 1 1 1 0 0 1 ; 0 1 1 0 1 0 ]
-the_codeword=[0 1 1 1 0 1];
 h_size=size(H);
-the_indicator_matrix=zeros(h_size(1),h_size(2));
+the_codeword=[0 1 1 1 0 1]
+the_cross_over_probability=[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9];
+errors=zeros(numel(the_cross_over_probability));
+'next is tcop looping'
+for tcop=1:numel(the_cross_over_probability)
+    '************************START*******************************'
+    tcop
+    the_bsc_codeword=bsc(the_codeword,the_cross_over_probability(tcop));
+    the_bsc_codeword
+    the_final_codeword=the_bsc_codeword;
 
-for i=1:h_size(1)
-    i_row_ones= find(H(i,:));
-    %find all the connections in to the checknode i
-    the_i_row_xor=0;
-    for k=1:numel(i_row_ones)
-        the_i_row_xor=xor(the_i_row_xor,the_codeword(i_row_ones(k)));
+    the_indicator_matrix=zeros(h_size(1),h_size(2));
+
+    for i=1:h_size(1)
+        i_row_ones= find(H(i,:));
+        %find all the connections in to the checknode i
+        the_i_row_xor=0;
+        for k=1:numel(i_row_ones)
+            the_i_row_xor=xor(the_i_row_xor,the_bsc_codeword(i_row_ones(k)));
+        end
+        for j=1:numel(i_row_ones)
+            the_indicator_matrix(i,i_row_ones(j))=xor(the_i_row_xor,the_bsc_codeword(i_row_ones(j)));
+        end
     end
-   for j=1:numel(i_row_ones)
-        the_indicator_matrix(i,i_row_ones(j))=xor(the_i_row_xor,the_codeword(i_row_ones(j)));
-   end
-end
-    the_indicator_matrix
     
-for i=1:h_size(2)
-    i_i_row_ones=find(H(:,i));
-    the_sum_of_the_ones=0;
-    for j=1:numel(i_i_row_ones)
-        the_sum_of_the_ones=the_sum_of_the_ones+the_indicator_matrix(i_i_row_ones(j),i);
-    end
-    if(rem(numel(i_i_row_ones),2)==0)
-         if((int8(numel(i_i_row_ones)/2))<the_sum_of_the_ones)
-             the_codeword(i)=1;
-         end
-         if((int8(numel(i_i_row_ones)/2))>the_sum_of_the_ones)
-             the_codeword(i)=0;
-         end
-    end
+    for i=1:h_size(2)
+        i_i_row_ones=find(H(:,i));
+        the_sum_of_the_ones=0;
+        for j=1:numel(i_i_row_ones)
+            the_sum_of_the_ones=the_sum_of_the_ones+the_indicator_matrix(i_i_row_ones(j),i);
+        end
+        if(rem(numel(i_i_row_ones),2)==0)
+             if((int8(numel(i_i_row_ones)/2))<the_sum_of_the_ones)
+                 the_final_codeword(i)=1;
+             end
+            if((int8(numel(i_i_row_ones)/2))>the_sum_of_the_ones)
+                 the_final_codeword(i)=0;
+            end
+        end
     
    
-    if(rem(numel(i_i_row_ones),2)==1)
-         if((int8(numel(i_i_row_ones)/2)-1)<the_sum_of_the_ones)
-             the_codeword(i)=1;
-         end
-         if((int8(numel(i_i_row_ones)/2)-1)>=the_sum_of_the_ones)
-             the_codeword(i)=0;
-         end
+        if(rem(numel(i_i_row_ones),2)==1)
+             if((int8(numel(i_i_row_ones)/2)-1)<the_sum_of_the_ones)
+                 the_final_codeword(i)=1;
+             end
+            if((int8(numel(i_i_row_ones)/2)-1)>=the_sum_of_the_ones)
+                 the_final_codeword(i)=0;
+            end
+        end
     end
+    
+    %calculate the number of errors
+    for o=1:h_size(2)
+        if(the_final_codeword(o)~=the_codeword(o))
+            errors(tcop)=errors(tcop)+1;
+        end
+    end
+    the_final_codeword
+    errors(tcop)
+    '************************END*******************************'
 end
-the_codeword
+plot(the_cross_over_probability,errors)
